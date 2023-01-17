92f99435800c0d678691b55ebb7732586586e89d
RoyJumah <royjumah17@gmail.com>
2023-01-17 16:49:40 +0300

    Install the chance and lorem packages to generate random qoutes and sentences

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,3 +1,11 @@
-const Home = () => <h1>Welcome to our Page!</h1>;
+const { loremIpsum } = require('lorem-ipsum');
+
+const Home = () => (
+  <div>
+    <h1>Welcome to our Page!</h1>
+    <p>{loremIpsum({ count: 1, units: 'paragraph' })}</p>
+    <p>{loremIpsum({ count: 1, units: 'paragraph' })}</p>
+  </div>
+);
 
 export default Home;
--- src/components/Pages/Quote.js
+++ src/components/Pages/Quote.js
@@ -1,10 +1,26 @@
-const Quote = () => (
-  <div>
-    <p>
-      Mathematics is not about numbers, equations, computation, or algorithms:
-      it is about understanding. &mdash; William Paul Thurston
-    </p>
-  </div>
-);
+import Chance from 'chance';
+
+const Quote = () => {
+  const chance = new Chance();
+  const mathQuotes = [
+    'Math is the language in which God has written the universe.',
+    "Without mathematics, there's nothing you can do. Everything around you is mathematics.",
+    'Math is like love; a simple idea, but it can get complicated',
+    'Math is the alphabet with which God has written the universe.',
+    'The beauty of mathematics only shows itself to more patient followers.',
+  ];
+  const quote = chance.pickone(mathQuotes);
+  const author = chance.name();
+  return (
+    <div>
+      <p>
+        {quote}
+        {' '}
+        &mdash;
+        {author}
+      </p>
+    </div>
+  );
+};
 
 export default Quote;
