8bd5c6af05bba2be1728c54fb8e9efe04d93e407
RoyJumah <royjumah17@gmail.com>
2023-01-17 16:13:47 +0300

    Add quote and calculator header to respective pages

--- src/components/Pages/Calculator.js
+++ src/components/Pages/Calculator.js
@@ -19,92 +19,96 @@ const Calculator = () => {
   const { total, next } = state;
 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">{total}</div>
-        <div className="current-operand">{next || total}</div>
+    <div className="display-flex__calculator">
+      <h1>Lets do some math!</h1>
+
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">{total}</div>
+          <div className="current-operand">{next || total}</div>
+        </div>
+        <button type="button" onClick={() => handleClick('AC')}>
+          AC
+        </button>
+        <button type="button" onClick={() => handleClick('+/-')}>
+          +/-
+        </button>
+        <button type="button" onClick={() => handleClick('%')}>
+          %
+        </button>
+        <button
+          type="button"
+          className="special__operators"
+          onClick={() => handleClick('÷')}
+        >
+          /
+        </button>
+        <button type="button" onClick={() => handleClick('7')}>
+          7
+        </button>
+        <button type="button" onClick={() => handleClick('8')}>
+          8
+        </button>
+        <button type="button" onClick={() => handleClick('9')}>
+          9
+        </button>
+        <button
+          type="button"
+          className="special__operators"
+          onClick={() => handleClick('x')}
+        >
+          x
+        </button>
+        <button type="button" onClick={() => handleClick('4')}>
+          4
+        </button>
+        <button type="button" onClick={() => handleClick('5')}>
+          5
+        </button>
+        <button type="button" onClick={() => handleClick('6')}>
+          6
+        </button>
+        <button
+          type="button"
+          className="special__operators"
+          onClick={() => handleClick('-')}
+        >
+          -
+        </button>
+        <button type="button" onClick={() => handleClick('1')}>
+          1
+        </button>
+        <button type="button" onClick={() => handleClick('2')}>
+          2
+        </button>
+        <button type="button" onClick={() => handleClick('3')}>
+          3
+        </button>
+        <button
+          type="button"
+          className="special__operators"
+          onClick={() => handleClick('+')}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="span-two"
+          onClick={() => handleClick('0')}
+        >
+          0
+        </button>
+        <button type="button" onClick={() => handleClick('.')}>
+          .
+        </button>
+        <button
+          type="button"
+          className="special__operators"
+          onClick={() => handleClick('=')}
+        >
+          =
+        </button>
       </div>
-      <button type="button" onClick={() => handleClick('AC')}>
-        AC
-      </button>
-      <button type="button" onClick={() => handleClick('+/-')}>
-        +/-
-      </button>
-      <button type="button" onClick={() => handleClick('%')}>
-        %
-      </button>
-      <button
-        type="button"
-        className="special__operators"
-        onClick={() => handleClick('÷')}
-      >
-        /
-      </button>
-      <button type="button" onClick={() => handleClick('7')}>
-        7
-      </button>
-      <button type="button" onClick={() => handleClick('8')}>
-        8
-      </button>
-      <button type="button" onClick={() => handleClick('9')}>
-        9
-      </button>
-      <button
-        type="button"
-        className="special__operators"
-        onClick={() => handleClick('x')}
-      >
-        x
-      </button>
-      <button type="button" onClick={() => handleClick('4')}>
-        4
-      </button>
-      <button type="button" onClick={() => handleClick('5')}>
-        5
-      </button>
-      <button type="button" onClick={() => handleClick('6')}>
-        6
-      </button>
-      <button
-        type="button"
-        className="special__operators"
-        onClick={() => handleClick('-')}
-      >
-        -
-      </button>
-      <button type="button" onClick={() => handleClick('1')}>
-        1
-      </button>
-      <button type="button" onClick={() => handleClick('2')}>
-        2
-      </button>
-      <button type="button" onClick={() => handleClick('3')}>
-        3
-      </button>
-      <button
-        type="button"
-        className="special__operators"
-        onClick={() => handleClick('+')}
-      >
-        +
-      </button>
-      <button
-        type="button"
-        className="span-two"
-        onClick={() => handleClick('0')}
-      >
-        0
-      </button>
-      <button type="button" onClick={() => handleClick('.')}>
-        .
-      </button>
-      <button
-        type="button"
-        className="special__operators"
-        onClick={() => handleClick('=')}
-      >
-        =
-      </button>
     </div>
   );
 };
--- src/components/Pages/Quote.js
+++ src/components/Pages/Quote.js
@@ -1,3 +1,10 @@
-const Quote = () => <h1>The Quote Page</h1>;
+const Quote = () => (
+  <div>
+    <p>
+      Mathematics is not about numbers, equations, computation, or algorithms:
+      it is about understanding. &mdash; William Paul Thurston
+    </p>
+  </div>
+);
 
 export default Quote;
